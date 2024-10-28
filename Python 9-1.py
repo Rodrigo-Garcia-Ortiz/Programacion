fa9e493c42dca7520b975d526bf5b7ff3d9352b4
Rodrigo-Garcia-Ortiz <rodrigo.garciaortiz24@campusfp.es>
2024-10-28 10:24:18 +0100

    Python Ejercicio 9

---
+++ Python 9-1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+array1 = np.array([2, 5, 7, 4, 3])
+array2 = np.array([6, 2, 8, 5, 1])
+
+print(f"Array 1: {array1}")
+print(f"Array 2: {array2}")
+
+resultado = array1 + array2
+
+print(f"Resultado: {resultado}")
---
+++ Python 9-3.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+array = np.array([2, 5, 7, 4, 3, 9, 3, 8, 1, 6])
+print(f"Array: {array}")
+
+resultado = np.mean(array)
+print(f"Resultado: {resultado}")
