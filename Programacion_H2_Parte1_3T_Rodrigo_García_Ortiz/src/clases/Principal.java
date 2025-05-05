e38dbbcc27735e8374cc46bd645be556b6e4d077
Rodrigo-Garcia-Ortiz <rodrigo.garciaortiz24@campusfp.es>
2025-05-05 13:59:02 +0200

    Add files via upload

--- Programacion_H2_Parte1_3T_Rodrigo_García_Ortiz/src/clases/Principal.java
+++ Programacion_H2_Parte1_3T_Rodrigo_García_Ortiz/src/clases/Principal.java
@@ -1,5 +1,6 @@
 package clases;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
@@ -10,35 +11,42 @@ public class Principal {
 		
 		Scanner obj = new Scanner(System.in);
 		
-		do { //Crea un bucle hasta que el usuario escoja la opción de salir
+		try { //Comprueba si el tipo de dato introducido es el adecuado
 			
-			System.out.println("1 – Ver películas");
-			System.out.println("2 – Salir");
-			System.out.println();
+			do { //Crea un bucle hasta que el usuario escoja la opción de salir
 				
-			System.out.println("Escoja una opción: ");
-			int opcion = obj.nextInt();
-			System.out.println();
-				
-			if (opcion == 1) { //Muestra las películas registradas
-					
-				f.verPeliculas();
-					
-			} else if (opcion == 2) { //Sale del programa
-					
-				break;
-					
-			} else {
-					
-				System.out.println("Opción no válida");
+				System.out.println("1 – Ver películas");
+				System.out.println("2 – Salir");
 				System.out.println();
 					
-			}	
+				System.out.println("Escoja una opción: ");
+				int opcion = obj.nextInt();
+				System.out.println();
+					
+				if (opcion == 1) { //Muestra las películas registradas
+						
+					f.verPeliculas();
+						
+				} else if (opcion == 2) { //Sale del programa
+						
+					System.out.println("Saliendo del programa");
+					break;
+						
+				} else {
+						
+					System.out.println("Opción no válida");
+					System.out.println();
+						
+				}	
+				
+			} while (true);
 			
-		} while (true);
+		} catch (InputMismatchException e) { //Indica que se ha introducido un tipo de dato erróneo
+			
+			System.out.println("Tipo de dato erróneo");
+			
+		}
 		
 		obj.close();  
-
-		System.out.println("Saliendo del programa");
 	}
 }
